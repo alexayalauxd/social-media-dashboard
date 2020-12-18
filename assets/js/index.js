5223e6b14b35b298abdc0d6ef3e70ca5465a7c70
Alex Ayala <66438806+alexayalauxd@users.noreply.github.com>
2020-12-18 01:06:05 -0600

    Finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,14 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     checkbox.setAttribute('checked', true);
 }
 
-checkbox.addEventListener('change', function(event) {
-        document.body.classList.toggle('is-dark-mode')
+// document.body.style.setProperty('');
+checkbox.addEventListener('change', function (event) {
+    // console.log(this.checked);
+    if (this.checked) {
+        document.body.classList.add('is-dark-mode');
+        document.body.classList.remove('is-light-mode');
+    } else {
+        document.body.classList.add('is-light-mode');
+        document.body.classList.remove('is-dark-mode');
+    }
 })
